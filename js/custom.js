145ddca43c3fc64b786652e4de50859e583144bc
DindieWestWood <drouet.dimitri@gmail.com>
2015-04-30 09:38:17 +0200

    add home project

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,30 @@ $(window).scroll(function() {
     }
 });
 
+$( window ).load(function() {
+    setAboutImage('about-background', "images/about-background.jpg", 'about', 'about-col');
+});
+
+$( window ).resize(function() {
+    setAboutImage('about-background', "images/about-background.jpg", 'about', 'about-col');
+});
+
+function setAboutImage(id, src, containerheight, containerwidth){
+    var oh = document.getElementById(containerheight).clientHeight;
+    var ow = document.getElementById(containerwidth).clientWidth;
+    var w = document.body.clientWidth;
+    var i = document.getElementById(id);
+    i.setAttribute("src", src);
+    if (w < 768){
+        i.style.position = "relative";
+        i.style.width = "100%";
+        i.style.height = "auto";
+    } else {
+        i.style.position = "absolute";
+        i.style.height = ""+oh+"px";
+        i.style.width = "auto";
+        i.style.top = "0";
+        i.style.left = "0";
+        i.style.clip = "rect(auto," + (ow) + "px,auto,auto)"; 
+    }
+}
