c1812ffd0481ed63e78891abb190e86dfee9f92c
Unknown <drouet.dimitri@gmail.com>
2017-11-27 16:24:46 +0100

    V2.1
    Adding project modal

--- js/interactivity.js
+++ js/interactivity.js
@@ -2,67 +2,18 @@
 /*global $, jQuery, alert*/
 $(document).ready(function () {
     "use strict";
+	function changeImage(obj,img) {
+		obj.src = img;
+	}
     
-    /*var states = {
-        IDLE: 1,
-        MENU: 2,
-        DETAIL: 3
-    },
-        state = states.IDLE;
-    
-    $('.menu').click(function (e) {
-        e.preventDefault();
-        switch (state) {
-        case states.IDLE:
-            state = states.MENU;
-            $(this).removeClass('is-closed').addClass('is-opened');
-            $('.collapsed-menu').fadeIn(400);
-            break;
-        case states.MENU:
-            state = states.IDLE;
-            $(this).addClass('is-closed').removeClass('is-opened');
-            $('.collapsed-menu').fadeOut(400);
-            break;
-        case states.DETAIL:
-            break;
-        }
+	 $('.thumb').click(function () {
+		var target = $(this).attr("data-target");
+		var large = $(this).attr("data-large");
+		changeImage(document.getElementById(target), large);
+		$(this).parent().parent().parent().children().removeClass("active");
+		$(this).parent().parent().addClass("active");
     });
-    
-    $('.menu-btn').click(function (e) {
-        e.preventDefault();
-        switch (state) {
-        case states.IDLE:
-            break;
-        case states.MENU:
-            state = states.IDLE;
-            $('.menu').addClass('is-closed').removeClass('is-opened');
-            $('.collapsed-menu').fadeOut(400);
-            break;
-        case states.DETAIL:
-            break;
-        }
-    });
-    
-    $('.scrolling-btn').click(function () {
-        var id = $(this).attr("data-id");
-        $('html, body').animate({
-            scrollTop: ($(id).offset().top)
-        }, 400);
-    });
-    
-    $('.navbar').hide();
-    
-    $(function () {
-        $(window).scroll(function () {
-            var dist = ($(window).height() - $('.navbar').height() - $('.splash-div').height()) / 2;
-            if ($(this).scrollTop() > dist) {
-                $('.navbar').fadeIn();
-                $('.splash-div').fadeOut();
-            } else {
-                $('.navbar').fadeOut();
-                $('.splash-div').fadeIn();
-            }
-        });
-    });*/
-    
+
+
+
 });
